859f2275b80ac7a391afe981864da073b5154cf4
TM0987 <69478162+TM0987@users.noreply.github.com>
2024-03-16 14:32:29 -0400

    location perms and cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,10 @@ import 'package:geolocator/geolocator.dart';
 
 void main() async {
   // Required to connect to Firebase Cloud Storage
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
 
   runApp(const MyApp());
 }
@@ -25,21 +26,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -50,16 +36,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -69,31 +45,33 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   // Default map to center at the University of Maryland
   static const _initialCenter = LatLng(38.9869, -76.9426);
-  Position? _currentPosition;
   MapOptions options = const MapOptions(initialCenter: _initialCenter);
 
+  late Position position;
+
+  Future<void> getPosition() async {
+    position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+  }
+
+  void _add() {
+    getPosition();
+    print('${position.latitude.toString()}, ${position.longitude.toString()}');
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => const Placeholder()));
+  }
+
   // Connect storage to FirebaseStorage instance
-  final storage = FirebaseStorage.instance;
+  // final storage = FirebaseStorage.instance;
 
   // Generate random markers for testing
   List<Marker> randomMarkers =
       generateMarkers(length: 7, center: _initialCenter);
 
-  // Method that sends user to add new item page
-  // Include any info from this page that needs to be sent to add page in this
-  // method.
-  void _add() {
-    Navigator.push(
-        context, MaterialPageRoute(builder: (context) => const Placeholder()));
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
