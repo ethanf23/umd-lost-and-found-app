092c2f061b78fcbff3a8b4e033ceac7c88ac2ed9
TM0987 <69478162+TM0987@users.noreply.github.com>
2024-03-22 22:05:27 -0400

    preview before uploading functionality

--- lib/add_item_h.dart
+++ lib/add_item_h.dart
@@ -11,12 +11,12 @@ class AddItemH extends StatefulWidget {
 }
 
 class _AddItemHState extends State<AddItemH> {
-  List<Reference> _uploadedFiles = [];
+  File? _uploadedFile;
 
   @override
   void initState() {
     super.initState();
-    getUploadedFiles();
+    //getUploadedFiles();
   }
 
   @override
@@ -28,66 +28,99 @@ class _AddItemHState extends State<AddItemH> {
         ),
         centerTitle: true,
       ),
-      body: _buildUI(),
-      floatingActionButton: _uploadMediaButton(context),
+      body: Center(
+          child: Column(children: [
+        _selectMediaButton(context),
+        _buildUI(),
+        _uploadMediaButton(context),
+      ])),
     );
+  }
+
+  Widget _selectMediaButton(BuildContext context) {
+    return MaterialButton(
+        onPressed: () async {
+          File? selectedImage = await getImageFromGallery();
+          if (selectedImage != null) {
+            setState(() {
+              _uploadedFile = selectedImage;
+            });
+          }
+        },
+        child: const Text(
+          "Select Image",
+        ));
   }
 
   Widget _uploadMediaButton(BuildContext context) {
     return MaterialButton(
       onPressed: () async {
-        File? selectedImage = await getImageFromGallery();
-        if (selectedImage != null) {
-          bool success = await uploadFileForUser(selectedImage);
+        // File? selectedImage = await getImageFromGallery();
+        if (_uploadedFile != null) {
+          bool success = await uploadFileForUser(_uploadedFile!);
           if (success) {
+            setState(() {
+              _uploadedFile = null;
+            });
             print("Success");
-            getUploadedFiles();
           }
         }
       },
-      child: const Icon(
-        Icons.upload,
+      child: const Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Upload Image"),
+          Icon(
+            Icons.upload,
+          ),
+        ],
       ),
     );
   }
 
   Widget _buildUI() {
-    if (_uploadedFiles.isEmpty) {
+    if (_uploadedFile == null) {
       return const Center(
         child: Text("No files uploaded yet."),
       );
-    }
-    return ListView.builder(
-      itemCount: _uploadedFiles.length,
-      itemBuilder: (context, index) {
-        Reference ref = _uploadedFiles[index];
-        return FutureBuilder(
-          future: ref.getDownloadURL(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListTile(
-                leading: Image.network(snapshot.data!),
-                title: Text(
-                  ref.name,
-                ),
-              );
-            }
-            return Container();
-          },
-        );
-      },
-    );
-  }
-
-  void getUploadedFiles() async {
-    List<Reference>? result = await getUsersUplodedFiles();
-    if (result != null) {
-      print(result.toString());
-      setState(
-        () {
-          _uploadedFiles = result;
-        },
+    } else {
+      return Center(
+        child: Image(
+          image: FileImage(_uploadedFile!),
+        ),
       );
     }
+    // return ListView.builder(
+    //   itemCount: _uploadedFiles.length,
+    //   itemBuilder: (context, index) {
+    //     Reference ref = _uploadedFiles[index];
+    //     return FutureBuilder(
+    //       future: ref.getDownloadURL(),
+    //       builder: (context, snapshot) {
+    //         if (snapshot.hasData) {
+    //           return ListTile(
+    //             leading: Image.network(snapshot.data!),
+    //             title: Text(
+    //               ref.name,
+    //             ),
+    //           );
+    //         }
+    //         return Container();
+    //       },
+    //     );
+    //   },
+    // );
   }
+
+  // void getUploadedFiles() async {
+  //   List<Reference>? result = await getUsersUplodedFiles();
+  //   if (result != null) {
+  //     print(result.toString());
+  //     setState(
+  //       () {
+  //         _uploadedFiles = result;
+  //       },
+  //     );
+  //   }
+  // }
 }
